e0e1b50c161d2c513b6d14ee45f54d686419ed7d
Katie West <katiewest820@gmail.com>
2017-11-07 15:30:09 -0800

    heroku-add

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const serverShut = require('http-shutdown')
 
 const jsonParser = bodyParser.json();
 const app = express();
